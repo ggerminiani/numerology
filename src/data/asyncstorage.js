b148674fea7405a77e110888ae6150a79de7ece4
GUSTAVO GERMINIANI <gugart@hotmail.com>
2021-05-18 17:23:00 -0300

    AsyncStorage

---
+++ src/data/asyncstorage.js
@@ -0,0 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const storeData = async (key, value) => {
+  try {
+    if (typeof value === "object") {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(key, jsonValue);
+    } else {
+      await AsyncStorage.setItem(key, value);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getData = async (key) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.error(e);
+  }
+};
