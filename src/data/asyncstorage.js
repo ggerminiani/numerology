c30319bbdc4d452d5bd061bc2e6595213957f4a4
ggerminiani <40411853+ggerminiani@users.noreply.github.com>
2021-05-19 00:24:13 -0300

    Storage

--- src/data/asyncstorage.js
+++ src/data/asyncstorage.js
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const storeData = async (key, value) => {
+  console.log("AsyncStorage", AsyncStorage);
   try {
     if (typeof value === "object") {
       const jsonValue = JSON.stringify(value);
@@ -16,6 +17,7 @@ export const storeData = async (key, value) => {
 export const getData = async (key) => {
   try {
     const jsonValue = await AsyncStorage.getItem(key);
+    console.log("jsonValue", jsonValue);
     return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (e) {
     console.error(e);
